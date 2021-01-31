21dec6d4569f23a6e40a613825b07f2ea4584641
ramilabbaszade <abbasovramil65@gmail.com>
2021-01-31 18:21:08 +0400

    ADDED: Landing_page + 404page

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { Container } from "reactstrap";
+import { Container, Col, Row } from "reactstrap";
 import Navi from "./Navi";
+import landing_image from "../../assets/illustrations/shipping-landing.png";
 
 const Landing = () => {
   return (
     <div>
       <Navi />
-        <h1>YeniKargo</h1>
+      <div className="bg-primary" style={{ height: "80vh" }}>
+        <Container className="pt-5">
+          <Row style={{alignItems:'center'}}>
+            <Col className="text-white">
+              <h1>YeniKargo</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro quisquam non similique, aperiam iure veniam et quam, ullam earum culpa necessitatibus ab, placeat quae minima assumenda adipisci cupiditate consequatur repudiandae!</p>
+            </Col>
+              <img
+                style={{ width: "30rem", height: "20rem" }}
+                src={landing_image}
+                alt="landing"
+              />
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 };
